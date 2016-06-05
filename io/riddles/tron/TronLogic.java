1a0ce211b74c1f0f8fa5083cc68b0de8d6aba972
joost <bitbucket@trendradar.nl>
2016-06-05 19:10:24 +0200

    Get rid of some null returns

--- io/riddles/tron/TronLogic.java
+++ io/riddles/tron/TronLogic.java
@@ -7,6 +7,7 @@ import io.riddles.boardgame.model.Board;
 import io.riddles.boardgame.model.Coordinate;
 import io.riddles.boardgame.model.Move;
 import io.riddles.boardgame.model.Piece;
+import io.riddles.game.exception.InvalidDataException;
 import io.riddles.game.io.IOResponse;
 import io.riddles.tron.TronPiece.PieceColor;
 import io.riddles.tron.TronPiece.PieceType;
@@ -15,7 +16,7 @@ import io.riddles.boardgame.model.Direction;
 
 public final class TronLogic {
 
-	public static Move MoveTransformer(TronState state, IOResponse r) {
+	public static Move MoveTransformer(TronState state, IOResponse r) throws InvalidDataException {
 		PieceColor c = state.getActivePieceColor();
 		
 		Coordinate coord1 = getLightcycleCoordinate(state, c);
@@ -23,28 +24,25 @@ public final class TronLogic {
 		Direction direction = StringToDirectionTranformer(r.getValue());
 		
 		if (coord1 == null) {
-			return null;
+			throw new InvalidDataException("No lightcycle found");
 		}
-		if (direction == null) {
-			return null;
-		} else {
-			switch (direction) {
-				case UP:
-					coord2 = new Coordinate(coord1.getX(), coord1.getY()-1);
-					break;
-				case RIGHT:
-					coord2 = new Coordinate(coord1.getX()+1, coord1.getY());
-					break;
-				case DOWN:
-					coord2 = new Coordinate(coord1.getX(), coord1.getY()+1);
-					break;
-				case LEFT:
-					coord2 = new Coordinate(coord1.getX()-1, coord1.getY());
-					break;
-				default:
-					coord2 = coord1;
-					break;
-			}
+
+		switch (direction) {
+			case UP:
+				coord2 = new Coordinate(coord1.getX(), coord1.getY()-1);
+				break;
+			case RIGHT:
+				coord2 = new Coordinate(coord1.getX()+1, coord1.getY());
+				break;
+			case DOWN:
+				coord2 = new Coordinate(coord1.getX(), coord1.getY()+1);
+				break;
+			case LEFT:
+				coord2 = new Coordinate(coord1.getX()-1, coord1.getY());
+				break;
+			default:
+				coord2 = coord1;
+				break;
 		}
 		return new Move(coord1, coord2);
 	}
@@ -54,8 +52,7 @@ public final class TronLogic {
 	 * @param args : Tronstate, PieceColor
 	 * @return : Coordinate or null;
 	 */
-	
-	public static Coordinate getLightcycleCoordinate(TronState state, PieceColor c) {
+	public static Coordinate getLightcycleCoordinate(TronState state, PieceColor c) throws InvalidDataException {
 		Board b = state.getBoard();
 		int size = b.size();
 		
@@ -68,10 +65,10 @@ public final class TronLogic {
     			}
     		}
     	}
-		return null;
+    	throw new InvalidDataException ("Lightcycle not found.");
 	}
 	
-	public static Coordinate transformCoordinate(Coordinate c, Direction d) {
+	public static Coordinate transformCoordinate(Coordinate c, Direction d) throws InvalidDataException {
 		switch (d) {
 			case UP:
 				return new Coordinate(c.getX(), c.getY()-1);
@@ -81,16 +78,26 @@ public final class TronLogic {
 				return new Coordinate(c.getX(), c.getY()+1);
 			case LEFT:
 				return new Coordinate(c.getX()-1, c.getY());
-			default:
-				return null;
 		}
+		throw new InvalidDataException("Direction unknown");
 	}
 	
-	public static Direction StringToDirectionTranformer(String s) {
-		return Direction.UP;
+	public static Direction StringToDirectionTranformer(String s) throws InvalidDataException {
+		s = s.toLowerCase();
+		switch (s) {
+			case "up":
+				return Direction.UP;
+			case "right":
+				return Direction.RIGHT;
+			case "down":
+				return Direction.DOWN;
+			case "left":
+				return Direction.LEFT;
+		}
+		throw new InvalidDataException("Direction unknown");
 	}
 	
-	public static Direction getCurrentDirection(TronState state) {
+	public static Direction getCurrentDirection(TronState state) throws InvalidDataException {
 		Optional<TronState> prevState = state.getPreviousState();
 		PieceColor c = state.getActivePieceColor();
 		Coordinate coord1 = getLightcycleCoordinate(state, c);
@@ -111,7 +118,7 @@ public final class TronLogic {
 		}
 		
 		/* No previous state, figure out direction by player position */
-		/* TODO: make this better */
+		/* TODO: this could be more sophisticated */
 		if (coord1.getX() < state.getBoard().size()/2) {
 			return Direction.RIGHT;
 		} else {
